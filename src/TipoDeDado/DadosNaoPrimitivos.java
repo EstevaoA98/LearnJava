65ef6faf95f09320db9718a87b0262db501b8206
Estevão Miranda <estevao.a98@gmail.com>
2025-02-13 22:40:35 -0300

    copilando dados primitivos

---
+++ src/TipoDeDado/DadosNaoPrimitivos.java
@@ -0,0 +1,7 @@
+package TipoDeDado;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) {
+
+    }
+}
--- src/TipoDeDado/DadosPrimitivos.java
+++ src/TipoDeDado/DadosPrimitivos.java
@@ -2,6 +2,7 @@ package TipoDeDado;
 
 public class DadosPrimitivos {
     public static void main(String[] args) {
+        //psvm=public static void main
         /* Dados primitivos:
         BOOLEAN->Boolean
         CHARACTER->Char
@@ -17,5 +18,17 @@ public class DadosPrimitivos {
         boolean solteiro = true;
         Long saldoBancario = 1000L;//Valor maximo do long: 9 223 372 036 854 775 807;
 
+        System.out.println("Idade: " + idade);//Comando para mostra no console System.out.println
+        System.out.println("Altura: " + altura);
+        System.out.println("Inicial: " + inicial);
+        System.out.println("Solteiro: " + solteiro);
+        System.out.println("Saldo bancario é: " + saldoBancario);
+
+        /*
+        * sout = System.out.println().
+        * soutv = System.out.println(variavel); -> ultima variavel declarada.
+        * soutf = System.out.println(variavel);-> primeira variavel declarada.
+        * control + R = compilar.
+         */
     }
 }
