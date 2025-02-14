968c60e1c490bc72bbb97e12cf5bd85c6c2ad112
Estevão Miranda <estevao.a98@gmail.com>
2025-02-13 22:20:49 -0300

    Dados primitivos Java

---
+++ src/TipoDeDado/DadosPrimitivos.java
@@ -0,0 +1,21 @@
+package TipoDeDado;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /* Dados primitivos:
+        BOOLEAN->Boolean
+        CHARACTER->Char
+        INTEGER->byte, short, int, long
+        FLOAT->float, double
+
+        Objetivo da aula: Criar um um
+        */
+
+        int idade = 26;//valor maximo no int: 2 147 483 647
+        double altura = 1.85;
+        char inicial = 'E';
+        boolean solteiro = true;
+        Long saldoBancario = 1000L;//Valor maximo do long: 9 223 372 036 854 775 807;
+
+    }
+}
