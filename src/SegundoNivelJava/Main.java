2208ca7b30845fb8567fed5cf1f4c0eec5a400e4
Estevão Miranda <estevao.a98@gmail.com>
2025-02-21 23:16:12 -0300

    Metodos e parametos 02

--- src/SegundoNivelJava/Main.java
+++ src/SegundoNivelJava/Main.java
@@ -16,6 +16,19 @@ public class Main {
         Maria.sintomas = "Gripe e dor no corpo";
         Maria.peso = 60;
 
+        System.out.println("Paciente: " + Maria.nome + "\n idade: " + Maria.idade + "\n Endereço: " + Maria.endereco + "\n Sintomas: " + Maria.sintomas + "\n Peso do paciente:");
+
+
+        Maria.convenio();
+        Maria.consultado();
+        String consultado = Maria.consultado();
+        System.out.println(consultado);
+        int horario = Maria.horarioAtendimento();
+        System.out.println(horario + ":00");
+
+
+        System.out.println("-----------------------------");
+
         Paciente Jose = new Paciente();
         Jose.nome = "Jose de Oliveira";
         Jose.idade = 59;
@@ -23,8 +36,12 @@ public class Main {
         Jose.sintomas = "Dor no estômago e diarreia";
         Jose.peso = 75;
 
-        System.out.println("Paciente: " + Maria.nome + "\n idade: " + Maria.idade + "\n Endereço: " + Maria.endereco + "\n Sintomas: " + Maria.sintomas + "\n Peso do paciente:");
-        System.out.println("-----------------------------");
+
         System.out.println("Paciente: " + Jose.nome + "\n idade: " + Jose.idade + "\n Endereço: " + Jose.endereco + "\n Sintomas: " + Jose.sintomas + "\n Peso do paciente:");
+       Jose.particular();
+        String atendido = Jose.consultado();
+        System.out.println(atendido);
+        int horas = Jose.horarioAtendimento();
+        System.out.println(horario + ":00");
     }
 }
--- src/SegundoNivelJava/Paciente.java
+++ src/SegundoNivelJava/Paciente.java
@@ -8,5 +8,25 @@ public class Paciente {
     String sintomas;
     int peso;
 
-}
+    //Metodo VOID não retorna valor.
 
+    public void convenio(){
+        System.out.println("Pagamento: Convenio");
+    }
+
+    public void particular(){
+        System.out.println("Pagamento: Particular");             //Metodo VOID nao precisa de retorno
+    }
+    //Metodo STRING retorna valor uma string.
+    String consultado(){
+        return "Consultado";
+    }
+    //Metotodo INT retorna valor um inteiro.
+    int horarioAtendimento(){
+        return 10;
+    }
+    /*int valor = consultado(int valorApagar){
+        return valorApagar - 300;  //300 é o valor da consulta
+
+    }*/
+}
