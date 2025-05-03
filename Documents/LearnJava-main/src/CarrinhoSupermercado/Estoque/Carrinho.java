e1f6e7e105e077f9a7c2e67686d98395ea38c6f6
Estevão Miranda <estevao.a98@gmail.com>
2025-05-02 23:29:46 -0300

    Adicionando e removendo do estoque

--- Documents/LearnJava-main/src/CarrinhoSupermercado/Estoque/Carrinho.java
+++ Documents/LearnJava-main/src/CarrinhoSupermercado/Estoque/Carrinho.java
@@ -27,5 +27,38 @@ public class Carrinho {
         System.out.println("Quantidade: " + produto.quantidade);
 
         System.out.println("Valor total de " + produto.nome + " no estoque: " + produto.valorTotalCarrinho());
+
+        System.out.println("Deseja 1 - adicionar ou 2 - remover produtos?");
+        int opcao = sc.nextInt();
+
+        if (opcao != 1 && opcao != 2) {
+            System.out.println("Opção inválida");
+            return;
+        }
+        if (opcao == 1) {
+            System.out.println("Digite a quantidade de produtos que deseja adicionar: ");
+            produto.adicionarProdutos(sc.nextInt());
+
+            System.out.println("---------- Informações do produto no estoque ----------");
+            System.out.println("Nome: " + produto.nome);
+            System.out.println("Preço: " + produto.preco);
+            System.out.println("Quantidade: " + produto.quantidade);
+
+            System.out.println("Valor total de " + produto.nome + " no estoque: " + produto.valorTotalCarrinho());
+        }
+
+        if (opcao == 2){
+            System.out.println("Digite a quantidade de produtos que deseja remover: ");
+            produto.removerProdutos(sc.nextInt());
+
+            System.out.println("---------- Informações do produto no estoque ----------");
+            System.out.println("Nome: " + produto.nome);
+            System.out.println("Preço: " + produto.preco);
+            System.out.println("Quantidade: " + produto.quantidade);
+
+            System.out.println("Valor total de " + produto.nome + " no estoque: " + produto.valorTotalCarrinho());
+        }
+        sc.close();
+
     }
 }
