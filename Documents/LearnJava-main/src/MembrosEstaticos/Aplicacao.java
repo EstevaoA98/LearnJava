70936b9e53a0341e8eb04000ece9e5e11d0c1fb7
Estevão Miranda <estevao.a98@gmail.com>
2025-05-06 23:01:44 -0300

    Instanciando o objeto

--- Documents/LearnJava-main/src/MembrosEstaticos/Aplicacao.java
+++ Documents/LearnJava-main/src/MembrosEstaticos/Aplicacao.java
@@ -1,5 +1,7 @@
 package MembrosEstaticos;
 
+import MembrosEstaticos.utilitario.Calculador;
+
 import java.util.Scanner;
 
 public class Aplicacao {
@@ -8,25 +10,18 @@ public class Aplicacao {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Calculador calc = new Calculador();
 
         System.out.println("Digite o valor do raio:");
         double raio = scanner.nextDouble();
 
-        double c = circunferencia(raio);
-        double v = volume(raio);
+        double c = calc.circunferencia(raio);
+        double v = calc.volume(raio);
 
         System.out.printf("Circunferência: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
         System.out.printf("PI: %.2f%n", PI);
 
         scanner.close();
-    }
-
-    public static double circunferencia(double raio) {
-        return 2 * PI * raio;
-    }
-
-    public static double volume(double raio) {
-        return (4 / 3) * PI * Math.pow(raio, 3);
     }
 }
---
+++ Documents/LearnJava-main/src/MembrosEstaticos/utilitario/Calculador.java
@@ -0,0 +1,13 @@
+package MembrosEstaticos.utilitario;
+
+public class Calculador {
+    public final double PI = 3.14159;
+
+    public double circunferencia(double raio) {
+        return 2.0 * PI * raio;
+    }
+
+    public double volume(double raio) {
+        return (4.0 / 3.0) * PI * Math.pow(raio, 3);
+    }
+}
